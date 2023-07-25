aabdaf398868f7a7f3f788a9dd7c10e730481de6
tatymoly <tatymoly728@gmail.com>
2023-07-25 15:38:13 -0500

    practice useState

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,70 @@
 "use client";
 
-import { Button } from "@mui/material";
-import MyButton from "../components/button";
+import { useState } from "react";
+import {
+  IconButton,
+  Button,
+  Switch,
+  List,
+  ListItem,
+  TextField,
+} from "@mui/material";
+import { AddCircle, RemoveCircle } from "@mui/icons-material";
 
 export default function Home() {
-  const handleOnClick = () => {
-    console.log("me hicieron click");
+  const [count, setCount] = useState(0);
+
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  const handleRemove = () => {
+    setCount(count - 1);
   };
-  const handleMaterialClick = () => {
-    console.log("hice click en el boton de material");
+
+  const handleAdd = () => {
+    setCount(count + 1);
   };
+
+  const handleEnabled = () => {
+    setIsEnabled(!isEnabled);
+  };
+
   return (
-    <main>
-      <Button onClick={handleMaterialClick} variant="contained">
-        Contained
-      </Button>
-      <MyButton handleOnClick={handleOnClick}>Mi boton personalizado</MyButton>
-    </main>
+    <div>
+      <h2>Basic Counter</h2>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <IconButton onClick={handleRemove} aria-label="remove" color="primary">
+          <RemoveCircle />
+        </IconButton>
+        <p>{count}</p>
+        <IconButton onClick={handleAdd} aria-label="add" color="primary">
+          <AddCircle />
+        </IconButton>
+      </div>
+      <h2>Toggle enabled</h2>
+      <div>
+        <Button onClick={handleEnabled} variant="contained">
+          Enable/Disable toggle
+        </Button>
+        <Switch disabled={!isEnabled} />
+      </div>
+      <h2>TODO List</h2>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "300px",
+          }}
+        >
+          <TextField size="small" label="Add task..." variant="outlined" />
+          <Button variant="contained">Add Todo</Button>
+        </div>
+        <List>
+          <ListItem disablePadding>Learn useState</ListItem>
+          <p>...</p>
+        </List>
+      </div>
+    </div>
   );
 }
