ff132c79b9c6a1f47314fba45d4ca9432998dc8f
Paulina <jose@MacBook-Pro-de-Jose.local>
2023-07-28 22:06:22 -0500

    creating a todo list

--- src/app/page.js
+++ src/app/page.js
@@ -1,70 +1,49 @@
-"use client";
+"use client"
 
-import { useState } from "react";
+import React from 'react'
+import { useState } from 'react';
 import {
-  IconButton,
   Button,
-  Switch,
   List,
   ListItem,
   TextField,
 } from "@mui/material";
-import { AddCircle, RemoveCircle } from "@mui/icons-material";
 
-export default function Home() {
-  const [count, setCount] = useState(0);
-
-  const [isEnabled, setIsEnabled] = useState(true);
-
-  const handleRemove = () => {
-    setCount(count - 1);
-  };
-
-  const handleAdd = () => {
-    setCount(count + 1);
-  };
-
-  const handleEnabled = () => {
-    setIsEnabled(!isEnabled);
-  };
+export default function TodoForm() {
+  
+  const [input, setInput] = useState('')
+  const[todos, setTodos] = useState([])
+  const handleClick = () => {
+    const newTodo = {
+      id: new Date().getTime(),
+      text: input,
+      completed: false,
+    }
+    setTodos([...todos].concat(newTodo))
+    setInput('')
+  }
 
   return (
     <div>
-      <h2>Basic Counter</h2>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <IconButton onClick={handleRemove} aria-label="remove" color="primary">
-          <RemoveCircle />
-        </IconButton>
-        <p>{count}</p>
-        <IconButton onClick={handleAdd} aria-label="add" color="primary">
-          <AddCircle />
-        </IconButton>
-      </div>
-      <h2>Toggle enabled</h2>
-      <div>
-        <Button onClick={handleEnabled} variant="contained">
-          Enable/Disable toggle
-        </Button>
-        <Switch disabled={!isEnabled} />
-      </div>
       <h2>TODO List</h2>
-      <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: "300px",
-          }}
-        >
-          <TextField size="small" label="Add task..." variant="outlined" />
-          <Button variant="contained">Add Todo</Button>
-        </div>
-        <List>
-          <ListItem disablePadding>Learn useState</ListItem>
-          <p>...</p>
-        </List>
-      </div>
+        <form> 
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: "300px",
+            }}
+          >
+            <TextField size="small" label="Add task..." variant="outlined" value = {input}
+              onChange = {(event) => setInput(event.target.value)} 
+            />   
+            <Button variant="contained" onClick= {() => handleClick()} >Add To do</Button>
+          </div>         
+          <List>
+            {todos.map((input) => <ListItem disablePadding>{input.text}</ListItem> )}          
+          </List>  
+        </form>          
     </div>
   );
 }
